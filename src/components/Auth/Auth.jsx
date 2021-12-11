6525d32d6181f8e1074c4032b3adde20ddfee844
michaWorku <mikias27worku@gmail.com>
2021-12-11 20:03:36 +0300

    server URL auth added

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -32,7 +32,7 @@ const Auth = () => {
     e.preventDefault();
     const { username, password, phoneNumber, avatarURL } = form;
     // const URL = "http://localhost:5000/auth";
-    const URL = 'https://hakimchat-16.herokuapp.com/'
+    const URL = 'https://hakimchat-16.herokuapp.com/auth'
 
     const {
       data: { token, userId, hashedPassword, fullName },
